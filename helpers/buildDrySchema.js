c91968600128e912cb624dd8a8431cd4d0b4ad8f
SwapnilSoni1i999 <swapnil.soni12345@gmail.com>
2021-10-01 02:39:12 +0530

    helpers: buildDry: loop for custom validators

--- helpers/buildDrySchema.js
+++ helpers/buildDrySchema.js
@@ -1,13 +1,19 @@
 const dry = require('drytypes')
+const Types = require('../types')
 
 module.exports = (schema) => {
     const drySchema = {}
+    const customTypes = Object.keys(Types)
     for (const key in schema) {
         const { type, required } = schema[key]
         drySchema[key] = dry[type.name]
-        if (required) {
+        if (customTypes.includes(type.tag)) {
+            drySchema[key] = Types[type.tag]
+        }
+        if (!required) {
             drySchema[key].union(dry.Undefined)
         }
     }
+    console.log('schema', drySchema)
     return drySchema
 }
