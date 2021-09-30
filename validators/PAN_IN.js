75e0a8b4d6f6fb52348b47b6eb7fdb8e31bb7689
Vinayraj Cheekoti <66634103+knoxrover@users.noreply.github.com>
2021-10-01 03:03:22 +0530

    Update and rename pan_IN.js to PAN_IN.js

--- validators/PAN_IN.js
+++ validators/PAN_IN.js
@@ -2,7 +2,7 @@ const dry = require('drytypes')
 
 module.exports = dry.makeDryType((x) => {
     if (typeof x !== 'string') {
-        return { success: false, message: 'Invalid email address!' }
+        return { success: false, message: 'Invalid PAN number!' }
     }
 
     let panRegex = /^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$/;
