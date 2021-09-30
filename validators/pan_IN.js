816954e3a654ee8fccb4a218de9851773e0fcc6d
Vinayraj Cheekoti <66634103+knoxrover@users.noreply.github.com>
2021-10-01 03:02:28 +0530

    PAN_IN validator
    Added simple regex for validating Indian Pan number

---
+++ validators/pan_IN.js
@@ -0,0 +1,15 @@
+const dry = require('drytypes')
+
+module.exports = dry.makeDryType((x) => {
+    if (typeof x !== 'string') {
+        return { success: false, message: 'Invalid email address!' }
+    }
+
+    let panRegex = /^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$/;
+    if (!x.match(panRegex)) {
+        return { success: false, message: 'Invalid Pan number, Recheck PAN ID number please!' }
+    }
+  
+    return { success: true }
+  
+}, 'PAN_IN')
