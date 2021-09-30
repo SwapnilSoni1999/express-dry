a032476a83f3f2e368efb082822466db17fc315e
Vinayraj Cheekoti <66634103+knoxrover@users.noreply.github.com>
2021-10-01 03:44:19 +0530

    Update Aadhaar_IN.js
    Changes in typecheck and input length validation

--- validators/Aadhaar_IN.js
+++ validators/Aadhaar_IN.js
@@ -1,15 +1,20 @@
 const dry = require('drytypes')
 
 module.exports = dry.makeDryType((x) => {
-    if (typeof x !== 'string') {
-        return { success: false, message: 'Invalid aadhaar number!' }
+    if (typeof x !== 'number' || typeof x !== 'string') {
+        return { success: false, message: 'Aadhaar number should either be a String or a Number!' }
+    }
+    
+        x = x.toString()
+    if (x.length < 12) {
+        return { success: false, message: 'Aadhaar number must be a 12 digit number!')
     }
 
-    let aadharRegex = /^[2-9]{1}[0-9]{3}\s{1}[0-9]{4}\s{1}[0-9]{4}$/;
+    const aadharRegex = /^[2-9]{1}[0-9]{3}\s{1}[0-9]{4}\s{1}[0-9]{4}$/;
     if (!x.match(aadharRegex)) {
-        return { success: false, message: 'Invalid aadhar number!' }
+        return { success: false, message: 'Invalid Aadhaar number!' }
     }
-  
+
     return { success: true }
-  
-}, 'aadhaarNumber')
+
+}, 'AadhaarNumber')
