d40281d5dc39b00a646336eba0f2d832696707d5
Kishan Modi <45737372+KishanModi@users.noreply.github.com>
2021-10-01 04:42:50 +0530

    Update dl.js

--- validators/dl.js
+++ validators/dl.js
@@ -5,7 +5,7 @@ module.exports = dry.makeDryType((x) => {
         return { success: false, message: 'Driving Licence number should be string!' }
     }
 
-    let dlRegex = /^(([A-Z]{2}[0-9]{2})( )|([A-Z]{2}-[0-9]{2}))((19|20)[0-9][0-9])[0-9]{7}$”/;
+    let dlRegex = /^(([A-Z]{2}[0-9]{2})( )|([A-Z]{2}-[0-9]{2}))((19|20)[0-9][0-9])[0-9]{7}$/;
     if (!x.match(dlRegex)) {
         return { success: false, message: 'Invalid Driving Licence number!, Please Recheck the DL number!' }
     }
