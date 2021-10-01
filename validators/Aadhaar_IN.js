85f689f762bd18125d03f353b92dd98d3827b676
Vivek Kairi <vivekkairi30oct@gmail.com>
2021-10-01 14:03:02 +0530

    Typo fix

--- validators/Aadhaar_IN.js
+++ validators/Aadhaar_IN.js
@@ -7,7 +7,7 @@ module.exports = dry.makeDryType((x) => {
     
     x = x.toString()
     if (x.length < 12) {
-        return { success: false, message: 'Aadhaar number must be a 12 digit number!')
+        return { success: false, message: 'Aadhaar number must be a 12 digit number!'}
     }
 
     const aadharRegex = /^[2-9]{1}[0-9]{3}\s{1}[0-9]{4}\s{1}[0-9]{4}$/;
