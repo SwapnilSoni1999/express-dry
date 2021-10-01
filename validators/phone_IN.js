3b1a9cf38cce0655052173fea11036e9b5310dc6
Mohd Salman Ansari <salman0ansariii@gmail.com>
2021-10-01 19:16:51 +0530

    add phone validator

--- validators/phone_IN.js
+++ validators/phone_IN.js
@@ -5,7 +5,7 @@ module.exports = dry.makeDryType((x) => {
   if (typeof x !== 'string') {
     return { success: false, message: 'Phone number should be string!' }
 }
-// const phoneRegex = /^(\+?91|0)?[6789]\d{9}$/;
+
 const phoneRegex = /(((^[\+,0][9][1])(((\s[0-9]{7,10})|(\S[0-9]{7,10}))|([-]\S[0-9]{7,10})))|((^[\+,0][2]{2,2})((\S[0-9]{7,8})|((([-])[0-9]{7,8})|(\s[0-9]{7,8})))))|(((^[6,7,8,9][0-9]{9,9}))|(^[0,\+](([9][1)|[6,7,8,9]))[0-9]{8,9}))/gm;
 
     if (!x.match(phoneRegex)) {
