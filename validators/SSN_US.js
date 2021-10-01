feaeeebe14da3fd5ebe9b11a05b680a683f96aff
Kishan Modi <45737372+KishanModi@users.noreply.github.com>
2021-10-01 12:51:18 +0530

    Update and rename ssn_us.js to SSN_US.js

--- validators/SSN_US.js
+++ validators/SSN_US.js
@@ -5,11 +5,11 @@ module.exports = dry.makeDryType((x) => {
         return { success: false, message: 'Social Security Number should be string!' }
     }
 
-    let ssnRegex = /^(?!666|000|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0{4})\\d{4}$/;
+    const ssnRegex = /^(?!666|000|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0{4})\\d{4}$/;
     if (!x.match(ssnRegex)) {
         return { success: false, message: 'Invalid SSN!, Please Recheck the SSN!' }
     }
 
     return { success: true, message:'You Have Entered Valid Social Security Number!' }
 
-}, 'SSN')
+}, 'SSN_US')
