1f9d21d52cdcc2cafb278ad8e4562a78f7eeeaf9
Chirag Panchal <63766309+chiragxd@users.noreply.github.com>
2021-10-01 13:05:16 +0530

    Update SIN_CA.js

--- validators/SIN_CA.js
+++ validators/SIN_CA.js
@@ -2,14 +2,14 @@ const dry = require('drytypes')
 
 module.exports = dry.makeDryType((x) => {
     if (typeof x !== 'string') {
-        return { success: false, message: 'Social Insurance Number should be string!' }
+        return { success: false, message: 'Social Insuarance Number should be string!' }
     }
 
-    const sinRegex = ^(\d{3}-\d{3}-\d{3})|(\d{9})$;
+    const sinRegex = /^(\d{3}-\d{3}-\d{3})|(\d{9})$/;
     if (!x.match(sinRegex)) {
-        return { success: false, message: 'Invalid SIN!, Please Recheck the SIN!' }
+        return { success: false, message: 'Invalid SIN number!, Please check your SIN number.' }
     }
-
-    return { success: true, message:'You Have Entered Valid Social Insuarance Number!' }
-
-}, 'SIN')
+  
+    return { success: true, message:'SIN number is VALID!'}
+  
+}, 'SIN_CA')
