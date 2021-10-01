d798be8c8f35560e9f6fac7803d09d5bfb3265c0
Anshoool <79800870+AnshulxD@users.noreply.github.com>
2021-10-01 09:28:46 +0530

    Update DRIVING_LC_IN.js

--- validators/DRIVING_LC_IN.js
+++ validators/DRIVING_LC_IN.js
@@ -8,12 +8,9 @@ module.exports = dry.makeDryType((x) => {
 		};
 	}
 
-	let INDrivingLCRegex =
-		/^(([A-Z]{2}[0-9]{2})( )|([A-Z]{2}-[0-9]{2}))((19|20)[0-9][0-9])[0-9]{7}$/g;
+	let INDrivingLCRegex = /^(([A-Z]{2}[0-9]{2})( )|([A-Z]{2}-[0-9]{2}))((19|20)[0-9][0-9])[0-9]{7}$/g;
 	if (!x.match(INDrivingLCRegex)) {
-		return {
-			success: false,
-			message: 'Invalid Driving License Number',
+		return {success: false,message: 'Invalid Driving License Number Recheck Driving License number please!',
 		};
 	}
 	return { success: true };
