6198b5f18cd6275f7a9308ad2436eb8f5c8bc54c
Ayush Jindal <ayushj1998@gmail.com>
2021-10-01 09:55:38 +0530

    Added new validators for GST& VoterID

--- types.js
+++ types.js
@@ -4,6 +4,8 @@ const EmailAddress = require('./validators/email');
 const PAN_IN = require('./validators/PAN_IN');
 const AadhaarNumber = require('./validators/Aadhaar_IN');
 const DrivingLC_IN = require('./validators/DRIVING_LC_IN');
+const GST_IN=require('./validators/GST_IN');
+const VoterID_IN=require('./validators/VoterID_IN');
 
 module.exports = {
 	ObjectId,
@@ -12,4 +14,6 @@ module.exports = {
 	PAN_IN,
 	AadhaarNumber,
 	DrivingLC_IN,
+	GST_IN,
+	VoterID_IN
 };
