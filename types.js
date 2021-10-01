05e5de7b410c01f70fdab4e8af534405a1db9345
Chirag Panchal <63766309+chiragxd@users.noreply.github.com>
2021-10-01 12:51:56 +0530

    Update types.js

--- types.js
+++ types.js
@@ -4,7 +4,7 @@ const EmailAddress = require('./validators/email');
 const PAN_IN = require('./validators/PAN_IN');
 const AadhaarNumber = require('./validators/Aadhaar_IN');
 const DrivingLC_IN = require('./validators/DRIVING_LC_IN');
-const SIN= require('./validators/SIN');
+const SIN= require('./validators/SIN_CA');
 
 module.exports = {
 	ObjectId,
@@ -13,5 +13,5 @@ module.exports = {
 	PAN_IN,
 	AadhaarNumber,
 	DrivingLC_IN,
-	SIN,
+	SIN_CA,
 };
