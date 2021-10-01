05a50e1bc2c9495460eb776b4753477193f697b6
Kishan Modi <45737372+KishanModi@users.noreply.github.com>
2021-10-01 12:37:26 +0530

    Update types.js

--- types.js
+++ types.js
@@ -4,7 +4,7 @@ const EmailAddress = require('./validators/email');
 const PAN_IN = require('./validators/PAN_IN');
 const AadhaarNumber = require('./validators/Aadhaar_IN');
 const DrivingLC_IN = require('./validators/DRIVING_LC_IN');
-const passport = require('./validators/passport.js');
+const passport = require('./validators/passport');
 
 module.exports = {
 	ObjectId,
@@ -13,5 +13,5 @@ module.exports = {
 	PAN_IN,
 	AadhaarNumber,
 	DrivingLC_IN,
-	passport,
+	Passport,
 };
