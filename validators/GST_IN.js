3129fdc29c28bb90889b9e1c711fd663ad72b7d7
Sourav Gain <souravgain605@gmail.com>
2021-10-02 12:50:08 +0530

    GST_REGEX typo fixed

--- validators/GST_IN.js
+++ validators/GST_IN.js
@@ -12,7 +12,7 @@ module.exports = dry.makeDryType((x) => {
 
     const GST_REGEX = /^([0-9]{2}[a-zA-Z]{5}[0-9]{4}[a-zA-Z]{1}[1-9A-Za-z]{1}[Z]{1}[0-9a-zA-Z]{1})|([0-9]{4}[a-zA-Z]{3}[0-9]{5}[UO]{1}[N][a-zA-Z0-9]{1})|([0-9]{2}[a-zA-Z]{4}[0-9]{5}[a-zA-Z]{1}[0-9]{1}[Z]{1}[0-9]{1})|([0-9]{4}[a-zA-Z]{3}[0-9]{5}[N][R][0-9a-zA-Z]{1})|([0-9]{2}[a-zA-Z]{4}[a-zA-Z0-9]{1}[0-9]{4}[a-zA-Z]{1}[1-9A-Za-z]{1}[D]{1}[0-9a-zA-Z]{1})|([0-9]{2}[a-zA-Z]{5}[0-9]{4}[a-zA-Z]{1}[1-9A-Za-z]{1}[C]{1}[0-9a-zA-Z]{1})|([9][9][0-9]{2}[a-zA-Z]{3}[0-9]{5}[O][S][0-9a-zA-Z]{1})$/;
 
-    if (!x.match(GST_Regex)) {
+    if (!x.match(GST_REGEX)) {
         return { success: false, message: 'Invalid GST number!' }
     }
 
